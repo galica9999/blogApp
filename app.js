12cfef8cafee5b66473b675b15b8c0d11b5f3477
Ryan MacLennan <ram1229@rit.edu>
2020-09-10 11:34:59 -0400

    initial app setup and test data added

---
+++ app.js
@@ -0,0 +1,48 @@
+var bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    express     = require('express'),
+    app         = express();
+
+const { StringDecoder } = require('string_decoder');
+
+// APP setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//Mongo setup
+mongoose.connect('mongodb://localhost:27017/blogApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//added test data
+// Blog.create({
+//     title:"Test Blog",
+//     image:"https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//     body: "this is my test blog, please dont be too harsh on it"
+// })
+
+// ROUTES
+app.get('/blogs', function(req,res){
+
+})
+
+
+
+
+
+app.listen(3000, 'localhost', function () {
+    console.log('server has started');
+});
