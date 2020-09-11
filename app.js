a59fb8d8794f4632d247ad70584293ae5fc44ba9
Ryan MacLennan <ram1229@rit.edu>
2020-09-10 20:23:01 -0400

    added show route and created show page to show selected post when clicked

--- app.js
+++ app.js
@@ -49,9 +49,31 @@ app.get('/blogs', function(req,res){
     })
 });
 
+app.get('/blogs/new', function(req,res){
+    res.render('new');
+})
 
+app.post('/blogs', function(req,res){
+    Blog.create(req.body.blog, function(err,newBlog){
+        if(err){
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+})
 
+//show route
+app.get('/blogs/:id', function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog:foundBlog});
+        }
+    })
 
+})
 
 app.listen(3000, 'localhost', function () {
     console.log('server has started');
