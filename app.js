4df8b8643eae9d90854a9c2ab9e94e06b953fd0f
Ryan MacLennan <ram1229@rit.edu>
2020-09-24 19:57:03 -0400

    Revert "added eslint"
    This reverts commit fc36c832d22427715b443a4f251480a7909c6dcc.

--- app.js
+++ app.js
@@ -1,114 +1,114 @@
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const expressSanitizer = require('express-sanitizer');
-const mongoose = require('mongoose');
-const express = require('express');
-
-const app = express();
+var bodyParser  = require('body-parser'),
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer'),
+    mongoose    = require('mongoose'),
+    express     = require('express'),
+    app         = express();
 
 const { StringDecoder } = require('string_decoder');
 
 // APP setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer);
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-// Mongo setup
+//Mongo setup
 mongoose.connect('mongodb://localhost:27017/blogApp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-  .then(() => console.log('Connected to DB!'))
-  .catch((error) => console.log(error.message));
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  image: String,
-  body: String,
-  created: { type: Date, default: Date.now },
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
 
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+app.get('/', function(req,res){
+    res.redirect('/blogs');
 });
 
-// index
-app.get('/blogs', (req, res) => {
-  Blog.find({}, (err, blogs) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('index', { blogs });
-    }
-  });
+//index
+app.get('/blogs', function(req,res){
+    Blog.find({}, function(err,blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {blogs:blogs});
+        }
+    })
 });
 
-// new
-app.get('/blogs/new', (req, res) => {
-  res.render('new');
+//new
+app.get('/blogs/new', function(req,res){
+    res.render('new');
+})
+
+//create
+app.post('/blogs', function(req,res){
+    req.body.blog.body = req.expressSanitizer(req.body.blog.body);
+    Blog.create(req.body.blog, function(err,newBlog){
+        if(err){
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+})
+
+//show route
+app.get('/blogs/:id', function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog:foundBlog});
+        }
+    })
+
+})
+
+//edit
+app.get('/blogs/:id/edit', function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('edit', {blog: foundBlog});
+        }
+    });
 });
 
-// create
-app.post('/blogs', (req, res) => {
-  req.body.blog.body = req.expressSanitizer(req.body.blog.body);
-  Blog.create(req.body.blog, (err, newBlog) => {
-    if (err) {
-      res.render('new');
-    } else {
-      res.redirect('/blogs');
-    }
-  });
+//update route
+app.put('/blogs/:id', function(req,res){
+    req.body.blog.body = req.expressSanitizer(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id)
+        }
+    })
 });
 
-// show route
-app.get('/blogs/:id', (req, res) => {
-  Blog.findById(req.params.id, (err, foundBlog) => {
-    if (err) {
-      res.redirect('/blogs');
-    } else {
-      res.render('show', { blog: foundBlog });
-    }
-  });
-});
+//destroy route
+app.delete('/blogs/:id', function(req,res){
+    Blog.findByIdAndRemove(req.params.id, function(err, deleted){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/blogs')
+        }
+    })
+})
 
-// edit
-app.get('/blogs/:id/edit', (req, res) => {
-  Blog.findById(req.params.id, (err, foundBlog) => {
-    if (err) {
-      res.redirect('/blogs');
-    } else {
-      res.render('edit', { blog: foundBlog });
-    }
-  });
-});
-
-// update route
-app.put('/blogs/:id', (req, res) => {
-  req.body.blog.body = req.expressSanitizer(req.body.blog.body);
-  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
-    if (err) {
-      res.redirect('/blogs');
-    } else {
-      res.redirect(`/blogs/${req.params.id}`);
-    }
-  });
-});
-
-// destroy route
-app.delete('/blogs/:id', (req, res) => {
-  Blog.findByIdAndRemove(req.params.id, (err, deleted) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/blogs');
-    }
-  });
-});
-
-app.listen(3000, 'localhost', () => {
-  console.log('server has started');
+app.listen(3000, 'localhost', function () {
+    console.log('server has started');
 });
