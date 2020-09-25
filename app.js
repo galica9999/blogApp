f807abd37f013901f31f1e9b04c76c70eaa9efb3
Ryan MacLennan <ram1229@rit.edu>
2020-09-24 20:30:15 -0400

    commiting new app.js

--- app.js
+++ app.js
@@ -1,212 +1,113 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const expressSanitizer = require('express-sanitizer');
-const mongoose = require('mongoose');
-const express = require('express');
+var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
 
-const app = express();
-=======
-var bodyParser  = require('body-parser'),
-    methodOverride = require('method-override'),
-    mongoose    = require('mongoose'),
-    express     = require('express'),
-    app         = express();
->>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
-=======
-var bodyParser  = require('body-parser'),
-    methodOverride = require('method-override'),
-    expressSanitizer = require('express-sanitizer'),
-    mongoose    = require('mongoose'),
-    express     = require('express'),
-    app         = express();
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
-
-const { StringDecoder } = require('string_decoder');
-
-// APP setup
-app.use(bodyParser.urlencoded({ extended: true }));
-<<<<<<< HEAD
-app.use(expressSanitizer);
-<<<<<<< HEAD
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-=======
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
->>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
-=======
-app.use(methodOverride("_method"));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
-//Mongo setup
-mongoose.connect('mongodb://localhost:27017/blogApp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Connected to DB!'))
-    .catch(error => console.log(error.message));
-
+// MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
     created: {type: Date, default: Date.now}
 });
-
 var Blog = mongoose.model("Blog", blogSchema);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
-=======
-//added test data
-// Blog.create({
-//     title:"Test Blog",
-//     image:"https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-//     body: "this is my test blog, please dont be too harsh on it"
-// })
+// RESTFUL ROUTES
 
-// ROUTES
-//index
-app.get('/', function(req,res){
-    res.redirect('/blogs');
->>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
-=======
-app.get('/', function(req,res){
-    res.redirect('/blogs');
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
+app.get("/", function(req, res){
+   res.redirect("/blogs"); 
 });
 
-//index
-app.get('/blogs', function(req,res){
-    Blog.find({}, function(err,blogs){
-        if(err){
-            console.log(err);
-        } else {
-            res.render('index', {blogs:blogs});
-        }
-    })
+// INDEX ROUTE
+app.get("/blogs", function(req, res){
+   Blog.find({}, function(err, blogs){
+       if(err){
+           console.log("ERROR!");
+       } else {
+          res.render("index", {blogs: blogs}); 
+       }
+   });
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// new
-app.get('/blogs/new', (req, res) => {
-  res.render('new');
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
 });
-=======
-=======
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
-//new
-app.get('/blogs/new', function(req,res){
-    res.render('new');
-})
-<<<<<<< HEAD
 
-//create
-app.post('/blogs', function(req,res){
-    Blog.create(req.body.blog, function(err,newBlog){
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create blog
+    console.log(req.body);
+    console.log("===========")
+    console.log(req.body);
+    Blog.create(req.body.blog, function(err, newBlog){
         if(err){
-            res.render('new');
+            res.render("new");
         } else {
-            res.redirect('/blogs');
-        }
-    })
-})
->>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
-=======
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
-
-//create
-app.post('/blogs', function(req,res){
-    req.body.blog.body = req.expressSanitizer(req.body.blog.body);
-    Blog.create(req.body.blog, function(err,newBlog){
-        if(err){
-            res.render('new');
-        } else {
-            res.redirect('/blogs');
-        }
-    })
-})
-
-//show route
-app.get('/blogs/:id', function(req,res){
-    Blog.findById(req.params.id, function(err, foundBlog){
-        if(err){
-            res.redirect('/blogs');
-        } else {
-            res.render('show', {blog:foundBlog});
-        }
-    })
-
-})
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-// update route
-app.put('/blogs/:id', (req, res) => {
-  req.body.blog.body = req.expressSanitizer(req.body.blog.body);
-  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
-    if (err) {
-      res.redirect('/blogs');
-    } else {
-      res.redirect(`/blogs/${req.params.id}`);
-    }
-  });
-=======
-//update route
-app.put('/blogs/:id', function(req,res){
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-        if(err){
-            res.redirect('/blogs');
-        } else {
-            res.redirect('/blogs/' + req.params.id)
-        }
-    })
->>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
-=======
-//edit
-app.get('/blogs/:id/edit', function(req,res){
-    Blog.findById(req.params.id, function(err, foundBlog){
-        if(err){
-            res.redirect('/blogs');
-        } else {
-            res.render('edit', {blog: foundBlog});
+            //then, redirect to the index
+            res.redirect("/blogs");
         }
     });
->>>>>>> parent of 56d5587... Revert "Revert "added eslint""
 });
 
-//update route
-app.put('/blogs/:id', function(req,res){
-    req.body.blog.body = req.expressSanitizer(req.body.blog.body);
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-        if(err){
-            res.redirect('/blogs');
-        } else {
-            res.redirect('/blogs/' + req.params.id)
-        }
-    })
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   })
 });
 
-//destroy route
-app.delete('/blogs/:id', function(req,res){
-    Blog.findByIdAndRemove(req.params.id, function(err, deleted){
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
         if(err){
-            console.log(err);
+            res.redirect("/blogs");
         } else {
-            res.redirect('/blogs')
+            res.render("edit", {blog: foundBlog});
         }
-    })
+    });
 })
 
-app.listen(3000, 'localhost', function () {
-    console.log('server has started');
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      }  else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
 });
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+   //destroy blog
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   })
+   //redirect somewhere
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING!");
+})
