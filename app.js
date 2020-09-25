d36d264a93f2526ff82e16b872f12f089405ef49
Ryan MacLennan <ram1229@rit.edu>
2020-09-24 20:03:17 -0400

    reverting back to previous commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const expressSanitizer = require('express-sanitizer');
@@ -5,15 +6,28 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express();
+=======
+var bodyParser  = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoose    = require('mongoose'),
+    express     = require('express'),
+    app         = express();
+>>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
 
 const { StringDecoder } = require('string_decoder');
 
 // APP setup
 app.use(bodyParser.urlencoded({ extended: true }));
+<<<<<<< HEAD
 app.use(expressSanitizer);
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+=======
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+>>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
 
 // Mongo setup
 mongoose.connect('mongodb://localhost:27017/blogApp', {
@@ -32,8 +46,22 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
+<<<<<<< HEAD
 app.get('/', (req, res) => {
   res.redirect('/blogs');
+=======
+//added test data
+// Blog.create({
+//     title:"Test Blog",
+//     image:"https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//     body: "this is my test blog, please dont be too harsh on it"
+// })
+
+// ROUTES
+//index
+app.get('/', function(req,res){
+    res.redirect('/blogs');
+>>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
 });
 
 // index
@@ -47,10 +75,28 @@ app.get('/blogs', (req, res) => {
   });
 });
 
+<<<<<<< HEAD
 // new
 app.get('/blogs/new', (req, res) => {
   res.render('new');
 });
+=======
+//new
+app.get('/blogs/new', function(req,res){
+    res.render('new');
+})
+
+//create
+app.post('/blogs', function(req,res){
+    Blog.create(req.body.blog, function(err,newBlog){
+        if(err){
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+})
+>>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
 
 // create
 app.post('/blogs', (req, res) => {
@@ -86,6 +132,7 @@ app.get('/blogs/:id/edit', (req, res) => {
   });
 });
 
+<<<<<<< HEAD
 // update route
 app.put('/blogs/:id', (req, res) => {
   req.body.blog.body = req.expressSanitizer(req.body.blog.body);
@@ -96,6 +143,17 @@ app.put('/blogs/:id', (req, res) => {
       res.redirect(`/blogs/${req.params.id}`);
     }
   });
+=======
+//update route
+app.put('/blogs/:id', function(req,res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id)
+        }
+    })
+>>>>>>> parent of 2ccf7c7... added santizer so script tags will be removed.  updated front page ui as well
 });
 
 // destroy route
